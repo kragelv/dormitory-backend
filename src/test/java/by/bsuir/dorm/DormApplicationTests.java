5d5630f3f74b03c39e1089caddc2daf507b46250
Vadim <kr.vadim29@gmail.com>
2024-02-24 22:15:22 +0300

    INIT: initial commit

---
+++ src/test/java/by/bsuir/dorm/DormApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.dorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
